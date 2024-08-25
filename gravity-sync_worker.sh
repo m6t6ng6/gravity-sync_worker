13e3d52479012f288f46188fa8f9b1dd002c4097
Fernando Bustamante <fernando.miguel.bustamante@gmail.com>
2024-08-25 16:57:56 -0300

    execution fix

--- gravity-sync_worker.sh
+++ gravity-sync_worker.sh
@@ -6,9 +6,9 @@ AUX=$(echo $(date +%b-%d-%Y_%HH%MM)"-ART")
 DATE=${AUX^^}
 IP=$(hostname -f | column -t -s "." | awk '{print $1}')"-"$(ip -4 -br a | grep -i "eth0\|vmbr0" | awk '{print $3}' | column -t -s "/" | awk '{print $1}')
 
-/etc/gravity-sync/gravity-sync compare
+gravity-sync compare
 sleep 20
-/etc/gravity-sync/gravity-sync
+gravity-sync
 
 JSON_SUCCESS=$(echo '{ "timestamp": "'"$DATE"'", "target_system": "'"$IP"'", "cli_message": "GRAVITY-SYNC COMPLETED" }')
 
